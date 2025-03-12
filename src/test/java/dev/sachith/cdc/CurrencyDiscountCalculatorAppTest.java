013c274cf3604dc5926f8d37cfc14050c7b8c90c
Sachith Lakmal <sachithlakmal@icloud.om>
2025-03-12 19:23:30 +0530

    Add main application and test classes for Currency Discount Calculator

---
+++ src/test/java/dev/sachith/cdc/CurrencyDiscountCalculatorAppTest.java
@@ -0,0 +1,17 @@
+package dev.sachith.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Sachith Lakmal
+ * @since 0.0.1
+ */
+@SpringBootTest
+class CurrencyDiscountCalculatorAppTest {
+
+    @Test
+    void loadContext() {
+    }
+
+}
