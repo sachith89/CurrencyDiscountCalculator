77c25056bb78d86701752c3c22c9dbaeb79a1175
Sachith Lakmal <sachithlakmal@icloud.om>
2025-04-22 11:30:42 +0530

    refactor: update CurrencyExchangeService and ShoppingCartService to accept API key

--- src/main/java/dev/sachith/cdc/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/dev/sachith/cdc/service/impl/ShoppingCartServiceImpl.java
@@ -2,11 +2,13 @@ package dev.sachith.cdc.service.impl;
 
 import dev.sachith.cdc.domain.entity.ShoppingCartEntity;
 import dev.sachith.cdc.domain.dto.InvoiceDto;
+import dev.sachith.cdc.domain.vo.ExchangeRates;
 import dev.sachith.cdc.service.CurrencyExchangeService;
 import dev.sachith.cdc.service.DiscountCalculateService;
 import dev.sachith.cdc.service.ShoppingCartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 /**
  * @author sachith
@@ -21,10 +23,11 @@ class ShoppingCartServiceImpl implements ShoppingCartService {
     // @Transactional
     // Uncomment this line when using a database and require transaction management
     @Override
-    public InvoiceDto generateInvoice(ShoppingCartEntity shoppingCartEntity) {
+    public InvoiceDto generateInvoice(ShoppingCartEntity shoppingCartEntity, String apiKey) {
 
         InvoiceDto invoiceDto = discountCalculateService.calculate(shoppingCartEntity);
-        return null;
+         currencyExchangeService.getAllData(apiKey);
+        return invoiceDto;
     }
 
 }
